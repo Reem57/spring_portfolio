dd9d9a485239e4ef2cde7b375f8fb0f5617cbb3f
Reem Ben Ishai <benishai.r@gmail.com>
2022-12-14 10:07:21 -0800

    FRQ 3

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/Calculator.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/Calculator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
+import java.lang.Math;
 
 /* In mathematics,
     an expression or mathematical expression is a finite combination of symbols that is well-formed
@@ -24,6 +25,8 @@ public class Calculator {
     private final Map<String, Integer> OPERATORS = new HashMap<>();
     {
         // Map<"token", precedence>
+        OPERATORS.put("SQRT", 1);
+        OPERATORS.put("^", 2);
         OPERATORS.put("*", 3);
         OPERATORS.put("/", 3);
         OPERATORS.put("%", 3);
@@ -45,6 +48,8 @@ public class Calculator {
         // original input
         this.expression = expression;
 
+        this.paraCheck();
+
         // parse expression into terms
         this.termTokenizer();
 
@@ -53,6 +58,23 @@ public class Calculator {
 
         // calculate reverse polish notation
         this.rpnToResult();
+    }
+
+    private void paraCheck() {
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < this.expression.length(); i++) {
+            if (this.expression.charAt(i) == '(') {
+                left++;
+            }
+            if (this.expression.charAt(i) == ')') {
+                right++;
+            }
+        }
+        if (left != right) {
+            throw new IllegalArgumentException("Mismatched parenthesis");
+        }
     }
 
     // Test if token is an operator
@@ -70,19 +92,20 @@ public class Calculator {
     // Compare precedence of operators.
     private Boolean isPrecedent(String token1, String token2) {
         // token 1 is precedent if it is greater than token 2
-        return (OPERATORS.get(token1) - OPERATORS.get(token2) >= 0) ;
+        return (OPERATORS.get(token1) - OPERATORS.get(token2) >= 0);
     }
 
-    // Term Tokenizer takes original expression and converts it to ArrayList of tokens
+    // Term Tokenizer takes original expression and converts it to ArrayList of
+    // tokens
     private void termTokenizer() {
         // contains final list of tokens
         this.tokens = new ArrayList<>();
 
-        int start = 0;  // term split starting index
-        StringBuilder multiCharTerm = new StringBuilder();    // term holder
+        int start = 0; // term split starting index
+        StringBuilder multiCharTerm = new StringBuilder(); // term holder
         for (int i = 0; i < this.expression.length(); i++) {
             Character c = this.expression.charAt(i);
-            if ( isOperator(c.toString() ) || isSeparator(c.toString())  ) {
+            if (isOperator(c.toString()) || isSeparator(c.toString())) {
                 // 1st check for working term and add if it exists
                 if (multiCharTerm.length() > 0) {
                     tokens.add(this.expression.substring(start, i));
@@ -107,8 +130,9 @@ public class Calculator {
         }
     }
 
-    // Takes tokens and converts to Reverse Polish Notation (RPN), this is one where the operator follows its operands.
-    private void tokensToReversePolishNotation () {
+    // Takes tokens and converts to Reverse Polish Notation (RPN), this is one where
+    // the operator follows its operands.
+    private void tokensToReversePolishNotation() {
         // contains final list of tokens in RPN
         this.reverse_polish = new ArrayList<>();
 
@@ -121,9 +145,8 @@ public class Calculator {
                     tokenStack.push(token);
                     break;
                 case ")":
-                    while (tokenStack.peek() != null && !tokenStack.peek().equals("("))
-                    {
-                        reverse_polish.add( tokenStack.pop() );
+                    while (tokenStack.peek() != null && !tokenStack.peek().equals("(")) {
+                        reverse_polish.add(tokenStack.pop());
                     }
                     tokenStack.pop();
                     break;
@@ -131,13 +154,14 @@ public class Calculator {
                 case "-":
                 case "*":
                 case "/":
+                case "^":
+                case "SQRT":
                 case "%":
                     // While stack
                     // not empty AND stack top element
                     // and is an operator
-                    while (tokenStack.size() > 0 && isOperator(tokenStack.peek()))
-                    {
-                        if ( isPrecedent(token, tokenStack.peek() )) {
+                    while (tokenStack.size() > 0 && isOperator(tokenStack.peek())) {
+                        if (isPrecedent(token, tokenStack.peek())) {
                             reverse_polish.add(tokenStack.pop());
                             continue;
                         }
@@ -146,7 +170,7 @@ public class Calculator {
                     // Push the new operator on the stack
                     tokenStack.push(token);
                     break;
-                default:    // Default should be a number, there could be test here
+                default: // Default should be a number, there could be test here
                     this.reverse_polish.add(token);
             }
         }
@@ -158,28 +182,52 @@ public class Calculator {
     }
 
     // Takes RPN and produces a final result
-    private void rpnToResult()
-    {
+    private void rpnToResult() {
         // stack is used to hold operands and each calculation
         Stack<Double> calcStack = new Stack<Double>();
 
         // RPN is processed, ultimately calcStack has final result
-        for (String token : this.reverse_polish)
-        {
+        for (String token : this.reverse_polish) {
             // If the token is an operator, calculate
-            if (isOperator(token))
-            {
+            if (isOperator(token)) {
                 // Pop the two top entries
 
                 // Calculate intermediate results
-                result = 0.0;
+                double x = calcStack.pop();
+                double y = 0;
+                if (!(token.equals("SQRT"))) {
+                    y = calcStack.pop();
+                }
 
-                // Push intermediate result back onto the stack
-                calcStack.push( result );
+                switch (token) {
+                    case "+":
+                        result = y + x;
+                        break;
+                    case "-":
+                        result = y - x;
+                        break;
+                    case "^":
+                        result = Math.pow(y, x);
+                        break;
+                    case "*":
+                        result = y * x;
+                        break;
+                    case "/":
+                        result = y / x;
+                        break;
+                    case "SQRT":
+                        result = Math.sqrt(x);
+                        break;
+                    case "%":
+                        result = y % x;
+                        break;
+                    default:
+                        break;
+                }
+                calcStack.push(result);
             }
             // else the token is a number push it onto the stack
-            else
-            {
+            else {
                 calcStack.push(Double.valueOf(token));
             }
         }
@@ -191,8 +239,15 @@ public class Calculator {
     public String toString() {
         return ("Original expression: " + this.expression + "\n" +
                 "Tokenized expression: " + this.tokens.toString() + "\n" +
-                "Reverse Polish Notation: " +this.reverse_polish.toString() + "\n" +
+                "Reverse Polish Notation: " + this.reverse_polish.toString() + "\n" +
                 "Final result: " + String.format("%.2f", this.result));
+    }
+
+    public String toJSON() {
+        String json = "{ \"Expression\": \"" + this.expression + "\", \"Tokenized Expression\": \""
+                + this.tokens.toString() + "\", \"Reverse Polish Notation\": \"" + this.reverse_polish
+                + "\", \"Final Result\": " + this.result + " }";
+        return json;
     }
 
     // Tester method
@@ -220,6 +275,16 @@ public class Calculator {
 
         Calculator divisionMath = new Calculator("300/200");
         System.out.println("Division Math\n" + divisionMath);
+
+        System.out.println();
+
+        Calculator powerMath = new Calculator("5 ^ 2");
+        System.out.println("Power Math\n" + powerMath);
+
+        System.out.println();
+
+        Calculator sqrtMath = new Calculator("SQRT 4");
+        System.out.println("Square Root Math\n" + sqrtMath);
 
     }
 }
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
@@ -0,0 +1,38 @@
+package com.nighthawk.spring_portfolio.mvc.calculator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class CalculatorApiController {
+
+    @GetMapping("/calculator")
+    public String getResult(
+            @RequestParam(name = "expression", required = false, defaultValue = "5 * 4") String expression,
+            Model model) {
+
+        // Returns jsonified result of expression with tokens and everything
+        Calculator newCalc = new Calculator(expression);
+        String result = newCalc.toString();
+        model.addAttribute("result", result);
+
+        return "calculator";
+    }
+
+    @GetMapping("/api/calculator/{expression}")
+    public ResponseEntity<String> getResult(@PathVariable String expression) {
+
+        // Returns jsonified result of expression with tokens and everything
+        Calculator newCalc = new Calculator(expression);
+        String result = newCalc.toJSON();
+        return new ResponseEntity<String>(result, HttpStatus.OK);
+
+        // Bad ID
+    }
+
+}
